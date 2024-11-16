3bb83f13f37c3fc8163abe5e7d3934a0f8871526
jacksonsr451 <jacksonsr451@gmail.com>
2024-11-16 00:14:14 -0300

    refactor: include cli

--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-import os
-import shutil
-
-
-class CustomInstallCommand(install):
-    def run(self):
-        install.run(self)
-        
-        source = os.path.join(os.path.dirname(__file__), 'jackson_easy_api', 'manage.py')
-        destination = os.path.join(os.getcwd(), 'manage.py')
-        
-        try:
-            if os.path.exists(source):
-                shutil.copy(source, destination)
-                print("Arquivo 'manage.py' copiado para a raiz do projeto.")
-            else:
-                print("O arquivo 'manage.py' não foi encontrado no pacote.")
-        except Exception as e:
-            print(f"Erro ao mover 'manage.py': {e}")
-
 
 setup(
     name="easy-api",
@@ -29,7 +8,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Jackson Severino da Rocha",
     author_email="jacksonsr451@gmail.com",
-    url="https://github.com/jacksonsr451/easy-api",
+    url="https://github.com/jacksonsr451/jackson_easy_api",
     packages=find_packages(where=".", exclude=["tests"]),
     include_package_data=True,
     install_requires=[
@@ -45,7 +24,9 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    cmdclass={
-        'install': CustomInstallCommand,
+    entry_points={
+        'console_scripts': [
+            'start=jackson_easy_api.cli:start_project',
+        ],
     },
 )
