16590e05cc8c48e92266717d7abfc449aec3b0a0
jacksonsr451 <jacksonsr451@gmail.com>
2024-11-16 01:34:23 -0300

    refactor: include dependencies

--- jackson_easy_api/cli.py
+++ jackson_easy_api/cli.py
@@ -3,15 +3,30 @@
 import os
 import shutil
 from pathlib import Path
+import subprocess
+import sys
+
+def install_dependencies():
+    try:
+        if os.path.exists('poetry.lock'):
+            print("Poetry detected. Installing dependencies using Poetry...")
+            subprocess.check_call([sys.executable, '-m', 'poetry', 'install'])
+            print("Dependencies installed successfully using Poetry.")
+        elif os.path.exists('requirements.txt'):
+            print("Poetry not detected. Installing dependencies using pip...")
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+            print("Dependencies installed successfully using pip.")
+        else:
+            print("No poetry.lock or requirements.txt found. Please ensure you're using a dependency manager.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error while installing dependencies: {e}")
 
 def start_project():
-    # Caminho do arquivo manage.py dentro do pacote
+    install_dependencies()
+    
     source = os.path.join(os.path.dirname(__file__), 'manage.py')
-
-    # Caminho de destino onde o manage.py será copiado para a raiz do projeto
     destination = os.path.join(Path.cwd(), 'manage.py')
 
-    # Verificar se o manage.py já existe na raiz do projeto
     if not os.path.exists(destination):
         try:
             shutil.copy(source, destination)
