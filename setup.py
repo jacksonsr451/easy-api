c0d91cbc8665fd6dc3b3372e7a99b9cf40cbc3db
jacksonsr451 <jacksonsr451@gmail.com>
2024-11-15 23:42:20 -0300

    refactor: include custon instalation

--- setup.py
+++ setup.py
@@ -1,20 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from setuptools.command.install import install
 import os
-import subprocess
+import shutil
 
-def post_install():
-    subprocess.call([os.path.join(os.path.dirname(__file__), 'post_install.py')])
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        
+        source = os.path.join(os.path.dirname(__file__), 'jackson_easy_api', 'manage.py')
+        destination = os.path.join(os.getcwd(), 'manage.py')
+        
+        try:
+            if os.path.exists(source):
+                shutil.copy(source, destination)
+                print("Arquivo 'manage.py' copiado para a raiz do projeto.")
+            else:
+                print("O arquivo 'manage.py' não foi encontrado no pacote.")
+        except Exception as e:
+            print(f"Erro ao mover 'manage.py': {e}")
+
 
 setup(
     name="easy-api",
-    version="0.1.3",
+    version="0.2.0",
     description="A FastAPI project generator",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author="Jackson Severino da Rocha",
     author_email="jacksonsr451@gmail.com",
     url="https://github.com/jacksonsr451/easy-api",
-    packages=['jackson_easy_api'],
+    packages=find_packages(where=".", exclude=["tests"]),
     include_package_data=True,
     install_requires=[
         "fastapi",
@@ -29,12 +45,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    data_files=[('', ['jackson_easy_api/manage.py'])],
-    entry_points={
-        'console_scripts': [
-            'post-install = post_install:move_manage_py',
-        ],
+    cmdclass={
+        'install': CustomInstallCommand,
     },
 )
-
-post_install()
